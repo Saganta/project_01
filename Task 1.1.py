fa97ca29fff648096b187aff7280b6fda31d0b25
Tatyana <126003534+Saganta@users.noreply.github.com>
2023-03-26 21:18:51 +0300

    Add files via upload

---
+++ Task 1.1.py
@@ -0,0 +1,2 @@
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+print(my_favorite_songs[0:14],my_favorite_songs[64:77],my_favorite_songs[16:30],my_favorite_songs[51:62])
